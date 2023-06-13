79868850dc1f29e8485c1872ae9c2f0e4af995f1
sherif <sherif.ma465@gmail.com>
2023-06-13 19:57:00 +0300

    update

--- base/views.py
+++ base/views.py
@@ -72,13 +72,14 @@ def home(request):
         )
     topics = Topic.objects.all()
     room_count = rooms.count()
-    context={'rooms':rooms , 'topics':topics , 'room_count':room_count}
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q)) # to get the messages of the rooms that have the topic name that contains the q
+    context={'rooms':rooms , 'topics':topics , 'room_count':room_count , 'room_messages':room_messages}
     return render(request,'base/home.html',context)
 
 def room(request,pk): 
     room = Room.objects.get(id=pk)
     # to get child attributes of the room we type ******room.childAttribute_set.all()******
-    room_messages = room.message_set.all().order_by('-created') # to get all the messages of that room /**_set.all()** mostly used in 1 to many relationships
+    room_messages = room.message_set.all() # to get all the messages of that room /**_set.all()** mostly used in 1 to many relationships
     participants = room.participants.all() # to get all the participants in the room /**.all()** mostly used in many to many relationships
     if request.method == 'POST': # to check if the user is sending a message
         message = Message.objects.create( # to create a message
@@ -91,6 +92,15 @@ def room(request,pk):
     context = {'room':room , 'room_messages':room_messages , 'participants':participants}
     return render(request,'base/room.html',context) 
 
+
+
+def userProfile(request,pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all() # to get all the rooms of that user
+    room_messages = user.message_set.all() # to get all the messages of that user
+    topics = Topic.objects.all()
+    context = {'user':user , 'rooms':rooms , 'room_messages':room_messages , 'topics':topics}
+    return render(request,'base/profile.html',context)
 # @login_required is used to check if the user is logged in or not
 # and if not then it redirects to the login page
 # also used as a restriction to some pages that only logged in users can access
@@ -100,7 +110,9 @@ def createRoom(request):
     if request.method == 'POST':
         form = RoomForm(request.POST)
         if form.is_valid():
-            form.save()
+            room = form.save(commit=False) # to not save the room yet / to access the room object / making an instance of the room
+            room.host = request.user # to get the user that is creating the room
+            room.save()
             return redirect('home')
     context={'form':form}
     return render(request,'base/room_form.html',context)
